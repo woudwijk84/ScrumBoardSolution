2aeb728cb382046f3b01448a818b6d0e99d07aac
Rienk Woudwijk <rienk.woudwijk@centric.eu>
2016-04-14 21:25:32 +0200

    inital commit

---
+++ ScrumBoard/Scripts/default.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="jquery-2.2.3.js" />
+/// <reference path="bootstrap.js" />
+
+
+$(document).ready(function () {
+    scrumNS.initialize();
+    $('#btnSubmit').on('click', scrumNS.addIssueToBoard);
+});
+
+(function () {
+    this.scrumNS = this.scrumNS || {};
+    var scrum = this.scrumNS;
+    var $columns = 4;
+    var $destination;
+
+    scrum.initialize = function () {
+
+    }
+
+    function Issue(title, description, points, status) {
+        $title = title;
+        $description = description;
+        $points = points;
+        $status = status;
+    }
+
+    scrum.addIssueToBoard = function () {
+
+    }
+
+})();
