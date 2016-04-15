cd8b3e565ed2ecab9b6ac4a545d935fa88576e52
Rienk Woudwijk <rienk.woudwijk@centric.eu>
2016-04-15 11:35:29 +0200

    Made drag & drop possible

--- ScrumBoard/Scripts/default.js
+++ ScrumBoard/Scripts/default.js
@@ -1,10 +1,8 @@
 ﻿/// <reference path="jquery-2.2.3.js" />
-/// <reference path="bootstrap.js" />
 
 
 $(document).ready(function () {
     scrumNS.initialize();
-    $('#btnSubmit').on('click', scrumNS.addIssueToBoard);
 });
 
 (function () {
@@ -12,9 +10,62 @@ $(document).ready(function () {
     var scrum = this.scrumNS;
     var $columns = 4;
     var $destination;
+    var $draggedIssue;
 
     scrum.initialize = function () {
+        $('#btnSubmit').on('click', scrum.addIssueToBoard);
+        mockIssue();
+        dragEvents();
+    }
 
+    function mockIssue() {
+        var mock = '<div id="mockIssue" class="issue" draggable="true"><h5>MOCK</h5>test</div>';
+        $('#backlogCol').append(mock);
+    }
+
+    function dragEvents() {
+        $('.issue').on('dragstart', dragging);
+        $('.issue').on('dragend', draggingEnded);
+        $('.scrumCol').on('dragenter', dragEnter);
+        $('.scrumCol').on('dragover', preventDefault);
+        $('.scrumCol').on('dragleave', dragLeave);
+        $('.scrumCol').on('drop', dropIssue);
+    }
+
+    function dragging(e) {
+        $(e.target).addClass('dragging');
+        $draggedIssue = $(e.target);
+    }
+
+    function draggingEnded(e) {
+        $(e.target).removeClass('dragging')
+    }
+
+    function dragEnter(e) {
+        preventDefault(e);
+        console.log(e);
+        $(e.target).addClass('issueDroppoint');
+    }
+
+    function dragLeave(e) {
+        preventDefault(e);
+        $(e.target).removeClass('issueDroppoint');
+
+    }
+
+    function preventDefault(e) {
+        e.preventDefault();
+
+    }
+
+    function dropIssue(e) {
+        var col = $(e.target);
+        if (col.hasClass('scrumCol')) {
+            $draggedIssue.detach();
+            $draggedIssue.appendTo(col);
+        }
+
+        col.removeClass('issueDroppoint');
     }
 
     function Issue(title, description, points, status) {
