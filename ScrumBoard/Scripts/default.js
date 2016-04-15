3a13ebcde6524e1253d97ac0493591233328d507
Rienk Woudwijk <rienk.woudwijk@centric.eu>
2016-04-15 16:31:02 +0200

    Added functions for indexedDB **not tested**

--- ScrumBoard/Scripts/default.js
+++ ScrumBoard/Scripts/default.js
@@ -1,5 +1,7 @@
 ﻿/// <reference path="jquery-2.2.3.js" />
 
+//window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.ms
+
 $(document).ready(function () {
     scrumNS.initialize();
 });
@@ -10,11 +12,16 @@ $(document).ready(function () {
     var $columns = 4;
     var $destination;
     var $draggedIssue;
+    var $currentIssue;
     var $issueCount = 0;
+    var $indexedDB = window.indexedDB;
+    //var openRequest = $indexedDB.open("scrumIssues", 1);
+    var $db;
 
     scrum.initialize = function () {
         console.log('init');
         $('#btnSubmit').on('click', scrum.addIssueToBoard);
+        initDB();
         //mockIssue();
     }
 
@@ -114,5 +121,62 @@ $(document).ready(function () {
             $('#dragHistory').append(errorMsg);
         }
     };
+
+    function printIsseus(issues) {
+        var html = '';
+        for (var i = 0; i < issues.length; i++) {
+            var key = issues[i].key;
+            var issue = issues[i].issue;
+            html += '<div id="issue' + i + '" class="issue" draggable="true">'+
+                    '<h5>' + issue.title + '</h5> ' +
+                    '<p>' + issue.description + '<br /> ' +
+                    '<b>' + issue.points + ' points</b></p></div>';
+        }
+    }
+
+    // #region IndexedDB
+
+    function initDB() {
+        var openRequest = $indexedDB.open("scrumIssues", 1);
+        openRequest.onupgradeneeded = function (response) {
+            response.currentTarget.transaction.objectStore("Issues", { keypath: 'id', autoIncrement: true });
+        }
+        //var db;
+
+        openRequest.onsuccess = function (response) {
+            $db = openRequest.result;
+        }
+
+        openRequest.onerror = function (response) {
+            alert("Errorcode: " + response.target.errorcode);
+        }
+    }
+
+    function getRequest() {
+        var db = $db || initDB();
+        var transaction = db.transaction('Issues', 'readonly');
+        var request = transaction.objectStore('Issues');
+        return request;
+    }
+
+    scrum.getIssues = function () {
+        $currentIssue = { key: null, issue: {} }
+        var request = getRequest();
+        var requestCursor = request.OpenCursor();
+        var issues = [];
+
+        requestCursor.onsuccess = function (response) {
+            var cursor = response.target.result;
+            if (!cursor) {
+                scrum.addIssueToBoard();
+                return;
+            }
+
+            issues.push({ key: cursor.key, issue: cursor.value });
+            cursor.continue();
+        }
+    }
+
+    // #endregion
 
 })();
